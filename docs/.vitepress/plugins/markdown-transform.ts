411c42bb5fc2f3bd54e1fec10557ca42368de351
Vida Xie <vida_2020@163.com>
2026-01-19 19:50:46 +0800

    refactor(docs): organize local plugin

--- docs/.vitepress/plugins/markdown-transform.ts
+++ docs/.vitepress/plugins/markdown-transform.ts
@@ -5,6 +5,49 @@ import { packages } from '@eslint-stylistic/metadata'
 import graymatter from 'gray-matter'
 
 export function MarkdownTransform(): Plugin {
+  const shortId = 'default'
+  const pkg = packages.find(p => p.shortId === shortId)!
+  const ruleMapping = pkg.rules.reduce((prev, cur) => {
+    prev[cur.name] = cur
+    return prev
+  }, {} as Record<string, RuleInfo>)
+
+  function addExperimentalTip(rule: RuleInfo) {
+    return `
+      > [!IMPORTANT]
+      > 🧪 This rule is an experimental rule, changes may not follow semver.
+      >
+      > Should prefix \`exp-\` when using. For example: \`${rule.ruleId}\`
+    `.split('\n').map(l => l.trim()).join('\n')
+  }
+
+  function removeHeadings(content: string) {
+    return content.trimStart().replace(/^# .*\n/, '')
+  }
+
+  function addDefaultOptions(content: string) {
+    // TODO
+    return content
+  }
+
+  function resolveLink(link: string) {
+    if (!URL.canParse(link) && !ruleMapping[link]) {
+      return `https://eslint.org/docs/latest/rules/${link}`
+    }
+
+    return link
+  }
+
+  function addExtraLinks(title: string, links?: string[]) {
+    if (!links?.length)
+      return
+
+    return [
+      `## ${title}`,
+      ...links.map(link => `- [${link}](${resolveLink(link)})`),
+    ].join('\n')
+  }
+
   return {
     name: 'local:markdown-transform',
     enforce: 'pre',
@@ -12,43 +55,16 @@ export function MarkdownTransform(): Plugin {
       if (!id.endsWith('README.md'))
         return null
 
-      const shortId = 'default'
-
       const ruleName = basename(dirname(id))
-
-      const pkg = packages.find(p => p.shortId === shortId)!
-
-      const ruleMapping = pkg.rules.reduce((prev, cur) => {
-        prev[cur.name] = cur
-        return prev
-      }, {} as Record<string, RuleInfo>)
       const rule = ruleMapping[ruleName]
 
       if (!rule)
         return null
 
-      let {
+      const {
         data,
         content,
       } = graymatter(code)
-
-      function resolveLink(link: string) {
-        if (!URL.canParse(link) && !ruleMapping[link]) {
-          return `https://eslint.org/docs/latest/rules/${link}`
-        }
-
-        return link
-      }
-
-      function extraLinks(title: string, links?: string[]) {
-        if (!links?.length)
-          return
-
-        return [
-          `## ${title}`,
-          ...links.map(link => `- [${link}](${resolveLink(link)})`),
-        ].join('\n')
-      }
 
       const {
         experimental,
@@ -56,22 +72,16 @@ export function MarkdownTransform(): Plugin {
 
       const sup = experimental ? 'experimental' : ''
 
-      const experimentalTip = `
-        > [!IMPORTANT]
-        > 🧪 This rule is an experimental rule, changes may not follow semver.
-        >
-        > Should prefix \`exp-\` when using. For example: \`${rule.ruleId}\`
-      `.split('\n').map(l => l.trim()).join('\n')
-
-      content = [
-        `# <samp>${rule.name}${sup ? ` <sup>${sup}</sup>` : ''}</samp>`,
-        experimental ? experimentalTip : '',
-        content.trimStart().replace(/^# .*\n/, ''),
-        extraLinks('Related Rules', data.related_rules),
-        extraLinks('Further Reading', data.further_reading),
-      ].join('\n')
-
-      return graymatter.stringify(content, { data })
+      return graymatter.stringify(
+        [
+          `# <samp>${rule.name}${sup ? ` <sup>${sup}</sup>` : ''}</samp>`,
+          experimental ? addExperimentalTip(rule) : '',
+          addDefaultOptions(removeHeadings(content)),
+          addExtraLinks('Related Rules', data.related_rules),
+          addExtraLinks('Further Reading', data.further_reading),
+        ].join('\n'),
+        { data },
+      )
     },
   }
 }
